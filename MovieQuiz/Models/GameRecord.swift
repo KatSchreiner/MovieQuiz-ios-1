f9e23e17e26daecfbc5fa315eb7d509966ba332d
KatSchreiner <kat.schreiner@yandex.ru>
2024-01-28 02:40:52 +0300

    Написан класс для ведения статистики

---
+++ MovieQuiz/Models/GameRecord.swift
@@ -0,0 +1,8 @@
+//
+//  GameRecord.swift
+//  MovieQuiz
+//
+//  Created by Екатерина Шрайнер on 26.01.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/StatisticService.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticService.swift
+//  MovieQuiz
+//
+//  Created by Екатерина Шрайнер on 26.01.2024.
+//
+
+import Foundation
