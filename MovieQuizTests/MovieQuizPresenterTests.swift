dfd9ea00b8840448bff5ce1b03908afb9dd26031
KatSchreiner <kat.schreiner@yandex.ru>
2024-02-20 19:54:43 +0300

    Доработки: отрефакторен MovieQuizViewController и написан тест для Presenter

--- MovieQuizTests/MovieQuizPresenterTests.swift
+++ MovieQuizTests/MovieQuizPresenterTests.swift
@@ -6,9 +6,20 @@
 //
 
 import XCTest
+@testable import MovieQuiz
+
+    final class MovieQuizViewControllerMock: MovieQuizViewControllerProtocol {
+        func showAlertGameEnd() {}
+        func clearBorder() {}
+        func enableButton() {}
+        func showActivityIndicator() {}
+        func hideActivityIndicator() {}
+        func show(quiz step: QuizStepViewModel) {}
+        func highlightImageBorder(isCorrectAnswer: Bool) {}
+        func showNetworkError(message: String) {}
+    }
 
 final class MovieQuizPresenterTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -32,4 +43,18 @@ final class MovieQuizPresenterTests: XCTestCase {
         }
     }
 
+
+    func testPresenterConvertModel() throws {
+        let viewControllerMock = MovieQuizViewControllerMock()
+        let sut = MovieQuizPresenter(viewController: viewControllerMock)
+        
+        let emptyData = Data()
+        let question = QuizQuestion(image: emptyData, text: "Question Text", correctAnswer: true)
+        let viewModel = sut.convert(model: question)
+        
+         XCTAssertNotNil(viewModel.image)
+        XCTAssertEqual(viewModel.question, "Question Text")
+        XCTAssertEqual(viewModel.questionNumber, "1/10")
+    }
 }
+
